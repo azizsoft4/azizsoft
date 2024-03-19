5360796b2276fe4d73d52b64c7086fbb24e9aa66
azizsoft4 <63382105+azizsoft4@users.noreply.github.com>
2024-03-19 18:50:32 +0100

    Update ajs.js

--- ajs.js
+++ ajs.js
@@ -1,3 +1,2 @@
 function s(e,a){return (typeof a === 'undefined')?document.querySelector(e):document.querySelectorAll(e);}
 function aj(l,d,f){let x=new XMLHttpRequest();x.onreadystatechange=function(){if(x.readyState==4&&x.status==200){f(x.response);}};x.open('POST',l,true);x.setRequestHeader('Content-type','application/x-www-form-urlencoded');x.send(Object.keys(d).map(function(key){return encodeURIComponent(key)+"="+encodeURIComponent(d[key]);}).join("&"));}
-document.addEventListener('click',function(e){if(!e.target.classList.contains('w')){document.querySelectorAll(".z:not(.o)").forEach((g) => {if(!e.composedPath().includes(g)){g.classList.add('o');}})}});
